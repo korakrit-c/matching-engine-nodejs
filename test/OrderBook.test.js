22fe06899bbac8eaa53f909a3ca5c15e9b57d5af
Korakrit Chariyasathian <13772625+korakrit-c@users.noreply.github.com>
2021-08-28 17:18:16 +0700

    Update OrderBook.test.js
    Create Test Data
    - data/[1-4]/input.json
    - data/[1-4]/output.json

--- test/OrderBook.test.js
+++ test/OrderBook.test.js
@@ -1,6 +1,8 @@
 import { expect } from 'chai';
+import * as fs from 'fs';
+import { sum } from "../utils/utils.js";
 import { OrderBook } from "../OrderBook.js";
-import { MatchingEngine } from '../MatchingEngine.js'
+import { MatchingEngine } from "../MatchingEngine.js";
 
 describe('OrderBook', () => {
     it('Import OrderBook', () => {
@@ -43,5 +45,209 @@ describe('MatchingEngine', () => {
         expect(matchEngine.orderBooks.sell).not.empty;
         expect(matchEngine.orderBooks.sell[price]).not.empty;
         expect(matchEngine.orderBooks.sell[price][0]).to.equal(amount);
+    });
+
+    it('[Data Set #1] Validate the volume of price should be equal as output.json', () => {
+        const matchEngine = new MatchingEngine();
+        // read file from input.json
+        let rawDataInput = fs.readFileSync('./data/1/input.json', 'utf8', (err, rawDataInput) => {});
+        let limitOrder = JSON.parse(rawDataInput);
+
+        // add order and then calculate with matching engine
+        for (let order of limitOrder.orders) {
+            matchEngine.addOrder(order.command, order.price, order.amount);
+        }
+
+        // read file from output.json
+        let rawDataOutput = fs.readFileSync('./data/1/output.json', 'utf8', (err, rawDataOutput) => {});
+        let outputJson = JSON.parse(rawDataOutput);
+
+        // expected at buy side
+        for (let orderBuy of outputJson.buy) {
+            let volumeFromFile = orderBuy.volume;
+            let volumeFromMatchEngine = sum(matchEngine.orderBooks.buy[orderBuy.price]);
+            expect(volumeFromMatchEngine).to.equal(volumeFromFile);
+        }
+        // expected at sell side
+        for (let orderSell of outputJson.sell) {
+            let volumeFromFile = orderSell.volume;
+            let volumeFromMatchEngine = sum(matchEngine.orderBooks.sell[orderSell.price]);
+            expect(volumeFromMatchEngine).to.equal(volumeFromFile);
+        }
+    });
+
+    it('[Data Set #2] Validate the volume of price should be equal as output.json', () => {
+        const matchEngine = new MatchingEngine();
+        // read file from input.json
+        let rawDataInput = fs.readFileSync('./data/2/input.json', 'utf8', (err, rawDataInput) => {});
+        let limitOrder = JSON.parse(rawDataInput);
+
+        // add order and then calculate with matching engine
+        for (let order of limitOrder.orders) {
+            matchEngine.addOrder(order.command, order.price, order.amount);
+        }
+
+        // read file from output.json
+        let rawDataOutput = fs.readFileSync('./data/2/output.json', 'utf8', (err, rawDataOutput) => {});
+        let outputJson = JSON.parse(rawDataOutput);
+
+        // expected at buy side
+        for (let orderBuy of outputJson.buy) {
+            let volumeFromFile = orderBuy.volume;
+            let volumeFromMatchEngine = sum(matchEngine.orderBooks.buy[orderBuy.price]);
+            expect(volumeFromMatchEngine).to.equal(volumeFromFile);
+        }
+        // expected at sell side
+        for (let orderSell of outputJson.sell) {
+            let volumeFromFile = orderSell.volume;
+            let volumeFromMatchEngine = sum(matchEngine.orderBooks.sell[orderSell.price]);
+            expect(volumeFromMatchEngine).to.equal(volumeFromFile);
+        }
+    });
+
+    it('[Data Set #3] Validate the volume of price should be equal as output.json', () => {
+        const matchEngine = new MatchingEngine();
+        // read file from input.json
+        let rawDataInput = fs.readFileSync('./data/3/input.json', 'utf8', (err, rawDataInput) => {});
+        let limitOrder = JSON.parse(rawDataInput);
+
+        // add order and then calculate with matching engine
+        for (let order of limitOrder.orders) {
+            matchEngine.addOrder(order.command, order.price, order.amount);
+        }
+
+        // read file from output.json
+        let rawDataOutput = fs.readFileSync('./data/3/output.json', 'utf8', (err, rawDataOutput) => {});
+        let outputJson = JSON.parse(rawDataOutput);
+
+        // expected at buy side
+        for (let orderBuy of outputJson.buy) {
+            let volumeFromFile = orderBuy.volume;
+            let volumeFromMatchEngine = sum(matchEngine.orderBooks.buy[orderBuy.price]);
+            expect(volumeFromMatchEngine).to.equal(volumeFromFile);
+        }
+        // expected at sell side
+        for (let orderSell of outputJson.sell) {
+            let volumeFromFile = orderSell.volume;
+            let volumeFromMatchEngine = sum(matchEngine.orderBooks.sell[orderSell.price]);
+            expect(volumeFromMatchEngine).to.equal(volumeFromFile);
+        }
+    });
+
+    it('[Data Set #4] Validate the volume of price should be equal as output.json', () => {
+        const matchEngine = new MatchingEngine();
+        // read file from input.json
+        let rawDataInput = fs.readFileSync('./data/4/input.json', 'utf8', (err, rawDataInput) => {});
+        let limitOrder = JSON.parse(rawDataInput);
+
+        // add order and then calculate with matching engine
+        for (let order of limitOrder.orders) {
+            matchEngine.addOrder(order.command, order.price, order.amount);
+        }
+
+        // read file from output.json
+        let rawDataOutput = fs.readFileSync('./data/4/output.json', 'utf8', (err, rawDataOutput) => {});
+        let outputJson = JSON.parse(rawDataOutput);
+
+        // expected at buy side
+        for (let orderBuy of outputJson.buy) {
+            let volumeFromFile = orderBuy.volume;
+            let volumeFromMatchEngine = sum(matchEngine.orderBooks.buy[orderBuy.price]);
+            expect(volumeFromMatchEngine).to.equal(volumeFromFile);
+        }
+        // expected at sell side
+        for (let orderSell of outputJson.sell) {
+            let volumeFromFile = orderSell.volume;
+            let volumeFromMatchEngine = sum(matchEngine.orderBooks.sell[orderSell.price]);
+            expect(volumeFromMatchEngine).to.equal(volumeFromFile);
+        }
+    });
+
+    it('[Data Set #1] Validate the JsonObject from a function should be equal as output.json', () => {
+        const matchEngine = new MatchingEngine();
+        // read file from input.json
+        let rawDataInput = fs.readFileSync('./data/1/input.json', 'utf8', (err, rawDataInput) => {});
+        let limitOrder = JSON.parse(rawDataInput);
+
+        // add order and then calculate with matching engine
+        for (let order of limitOrder.orders) {
+            matchEngine.addOrder(order.command, order.price, order.amount);
+        }
+
+        // read file from output.json
+        let rawDataOutput = fs.readFileSync('./data/1/output.json', 'utf8', (err, rawDataOutput) => {});
+        let outputJson = JSON.parse(rawDataOutput);
+
+        // printOrder
+        let ObjectJson = matchEngine.printOrder();
+        
+        // expected JsonObject to equal output.json
+        expect(JSON.stringify(ObjectJson)).to.equal(JSON.stringify(outputJson));
+    });
+
+    it('[Data Set #2] Validate the JsonObject from a function should be equal as output.json', () => {
+        const matchEngine = new MatchingEngine();
+        // read file from input.json
+        let rawDataInput = fs.readFileSync('./data/2/input.json', 'utf8', (err, rawDataInput) => {});
+        let limitOrder = JSON.parse(rawDataInput);
+
+        // add order and then calculate with matching engine
+        for (let order of limitOrder.orders) {
+            matchEngine.addOrder(order.command, order.price, order.amount);
+        }
+
+        // read file from output.json
+        let rawDataOutput = fs.readFileSync('./data/2/output.json', 'utf8', (err, rawDataOutput) => {});
+        let outputJson = JSON.parse(rawDataOutput);
+
+        // printOrder
+        let ObjectJson = matchEngine.printOrder();
+        
+        // expected JsonObject to equal output.json
+        expect(JSON.stringify(ObjectJson)).to.equal(JSON.stringify(outputJson));
+    });
+
+    it('[Data Set #3] Validate the JsonObject from a function should be equal as output.json', () => {
+        const matchEngine = new MatchingEngine();
+        // read file from input.json
+        let rawDataInput = fs.readFileSync('./data/3/input.json', 'utf8', (err, rawDataInput) => {});
+        let limitOrder = JSON.parse(rawDataInput);
+
+        // add order and then calculate with matching engine
+        for (let order of limitOrder.orders) {
+            matchEngine.addOrder(order.command, order.price, order.amount);
+        }
+
+        // read file from output.json
+        let rawDataOutput = fs.readFileSync('./data/3/output.json', 'utf8', (err, rawDataOutput) => {});
+        let outputJson = JSON.parse(rawDataOutput);
+
+        // printOrder
+        let ObjectJson = matchEngine.printOrder();
+        
+        // expected JsonObject to equal output.json
+        expect(JSON.stringify(ObjectJson)).to.equal(JSON.stringify(outputJson));
+    });
+
+    it('[Data Set #4] Validate the JsonObject from a function should be equal as output.json', () => {
+        const matchEngine = new MatchingEngine();
+        // read file from input.json
+        let rawDataInput = fs.readFileSync('./data/4/input.json', 'utf8', (err, rawDataInput) => {});
+        let limitOrder = JSON.parse(rawDataInput);
+
+        // add order and then calculate with matching engine
+        for (let order of limitOrder.orders) {
+            matchEngine.addOrder(order.command, order.price, order.amount);
+        }
+
+        // read file from output.json
+        let rawDataOutput = fs.readFileSync('./data/4/output.json', 'utf8', (err, rawDataOutput) => {});
+        let outputJson = JSON.parse(rawDataOutput);
+
+        // printOrder
+        let ObjectJson = matchEngine.printOrder();
+        
+        // expected JsonObject to equal output.json
+        expect(JSON.stringify(ObjectJson)).to.equal(JSON.stringify(outputJson));
     });
 });
