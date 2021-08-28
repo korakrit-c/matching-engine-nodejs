b5d6d88c4986fbbac229737f193aeb3d95c8d185
Korakrit Chariyasathian <13772625+korakrit-c@users.noreply.github.com>
2021-08-28 17:16:53 +0700

    Create utils/utils.js

---
+++ utils/utils.js
@@ -0,0 +1,27 @@
+import BigNumber from "bignumber.js";
+
+export const isEmptyObject = (obj) => {
+    return !Object.keys(obj).length;
+}
+
+export const getMax = (obj) => {
+    const arr = Object.keys(obj);
+    return BigNumber.max.apply(null, arr).toNumber() || null;
+}
+
+export const getMin = (obj) => {
+    const arr = Object.keys(obj);
+    return BigNumber.min.apply(null, arr).toNumber() || null;
+}
+
+export const sum = (arr) => {
+    return BigNumber.sum.apply(null, arr).toNumber();
+}
+
+export const minus = (x, y) => {
+    return BigNumber(x).minus(y).toNumber();
+}
+
+export const abs = (x) => {
+    return BigNumber(x).abs().toNumber();
+}
